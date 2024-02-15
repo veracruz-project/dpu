c5d171ed6202174b878bcfd3065636fa096760bf
Mohamed Husain <mohamedhusain.noormohamed@arm.com>
2024-02-15 10:31:50 -0600

    Edit endorsement script for non-root user

--- docker/attested-tls/attester/endorse.sh
+++ docker/attested-tls/attester/endorse.sh
@@ -2,10 +2,12 @@
 
 set -xeuf -o pipefail
 
+export PATH=$PATH:/usr/local/go/bin:/root/go/bin
+
 # Create AIK endorsement
 mkdir -p endorsement/comid
-parsec-tool create-endorsement -c "D10E4BD6-7E02-4D2C-BF1A-69AE22680478" > endorsement/comid-key.json
-cp ~/comid-pcr.json ~/corim.json endorsement/
+PARSEC_SERVICE_ENDPOINT=unix:/tmp/parsec.sock parsec-tool create-endorsement -c "D10E4BD6-7E02-4D2C-BF1A-69AE22680478" > endorsement/comid-key.json
+cp ./comid-pcr.json ./corim.json endorsement/
 
 # Create the endorsement bundle and endorse
 pushd endorsement
